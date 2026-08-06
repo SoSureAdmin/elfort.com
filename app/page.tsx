12ef62a11cc893b02aeef6dfa4bae079eb4a860b
Daniel Elfort <daniel@sosure.us>
2026-08-06 14:59:55 +0200

    Complete Elfort brand identity with heritage monogram

--- app/page.tsx
+++ app/page.tsx
@@ -136,10 +136,10 @@ export default function Home() {
               className="transition-opacity duration-300 hover:opacity-80"
             >
               <Image
-                src="/brand/monogram-gold.svg"
+                src="/brand/monogram-gold.png"
                 alt="Elfort"
-                width={42}
-                height={42}
+                width={46}
+                height={46}
               />
             </a>
 
