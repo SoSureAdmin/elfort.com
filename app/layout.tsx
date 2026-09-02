9310ff89a53e442ab12fcd3337295025ce491e3b
Daniel Elfort <daniel@sosure.us>
2026-09-02 15:36:12 +0200

    Add Google Analytics to elfort.com

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -40,6 +41,19 @@ export default function RootLayout({
     >
       <body className="min-h-full flex flex-col">
         {children}
+        <Script
+  src="https://www.googletagmanager.com/gtag/js?id=G-1QQMGKJ752"
+  strategy="afterInteractive"
+/>
+
+<Script id="google-analytics" strategy="afterInteractive">
+  {`
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-1QQMGKJ752');
+  `}
+</Script>
       </body>
     </html>
   );
